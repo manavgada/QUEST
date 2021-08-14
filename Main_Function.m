be50240fddfe970f8d17daf13de891b92b8d6115
manavgada <gadamanav@gmail.com>
2021-08-14 15:56:25 +0530

    Minor File Changes

--- Main_Function.m
+++ Main_Function.m
@@ -1,52 +1,30 @@
-Main()
-%%
-function Main()
+%Asks for the number of measurements made.
+k = readmatrix("input.xlsx","Range",'A1:A1');
+%Asks for the vectors in reference frame.
+%Value of t required to select AOR in SQR.
+t = readmatrix("input.xlsx","Range",'A2:A2');
+%Asks for weigts if any.
+w = readmatrix("input.xlsx","Range",strcat('B1:B',string(k)));
+%Asks for the vectors in Reference Frame.
+r = readmatrix("input.xlsx","Range",strcat('C1:E',string(k)));
+%Asks for the vectors in Body Frame.
+b = readmatrix("input.xlsx","Range",strcat('F1:H5',string(k)));
+%Asks for error parameter for sqr comparison.
+error = readmatrix("input.xlsx","Range",'A3:A3');
     
-    %Arbitrary value of t required to select AOR in SQR.
-    t = 1;
-    
-    while (true)
-        
-        %Arrangement to end the program.
-        if (input('Press -1 to exit: ') == -1)
-            break;
-        end
-    
-        %Asks for the number of measurements made.
-        k = input('Enter the number of vectors available: ');
-        %Asks for the vectors in reference frame.
-        r = zeros(3,k);
-        for i = 1:k
-            for j = 1:3
-                r(i,j) = input('Reference Frame Vector '+string(i)+' -> '+'Component'+string(j)+': ');
-            end
-        end
-        %Asks for the vectors in Body Frame.
-        b = zeros(3,k);
-        for i = 1:k
-            for j = 1:3
-                b(i,j) = input('Body Frame Vector '+string(i)+' -> '+'Component'+string(j)+': ');
-            end
-        end
-        %Asks for weigts if any.
-        w = zeros(1,k);
-        for i = 1:k
-            w(i) = input('Weightage Of Vector '+string(i)+': ');
-        end
-    
-        %Check for Singularity Case (180 degree rotation case).
-        if (abs(det(S2(r,b,w,k))))<0.01
-            finalq = sqrot(r,b,w,k,t);
-        else
-            finalq = q(r,b,w,k);
-        end
-    
-        %Purpose of our program.
-        disp(finalq);
-        
-        %SQR 't' updation.
-        t = sqrupdate(finalq);
-   
-    end
-    
+%Check for Singularity Case (180 degree rotation case).
+a = checkmatrix(r,b,w,k);
+if (checkmatrix(r,b,w,k)<error)
+    disp("Sqr Invoked");
+    finalq = sqrot(r,b,w,k,t);
+else
+    finalq = q(r,b,w,k);
 end
+    
+%SQR 't' updation.
+writematrix(sqrupdate(finalq),"input.xlsx",'Range','I1:I1');
+    
+%Purpose of our program.
+writematrix(finalq,"output.xlsx",'Range','B1:B4');
+writematrix(finalq,"input.xlsx",'Range','I1:I4');
+disp(finalq);
